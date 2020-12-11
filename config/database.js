a8a0f5e6556276aad750635d331f51b9156605ec
Darlison <Darlison001@gmail.com>
2020-12-11 20:49:41 -0300

    criando DB nopostgres

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 module.exports = {
     host: 'localhost',
     dialect: process.env.DIALECT_DB,
+    database: process.env.NAME_DB,
     username: process.env.USER_DB,
     password: process.env.PASSWORD_DB,
-    database: process.env.NAME_DB,
     port: process.env.PORT_DB,
     define: {
         timestamps: true,
