9f3601af60fc467cea656bafda8e5324ea013bf9
Darlison <Darlison001@gmail.com>
2020-12-11 23:53:03 -0300

    concluido Usercontrolles

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,5 +15,30 @@ module.exports = {
             console.log(error)
             return res.status(500).json(error.massage)
         }
+    },
+    async destroy(req, res) {
+        try {
+            const { id } = await req.params
+            const userExist = await user.findOne({
+                where: {
+                    id: id,
+                    active: true
+                }
+            })
+            if (!userExist) {
+                return res.status(400).json('User does not exist')
+            }
+            user.update(
+                {
+                    active: false
+                },
+                {
+                   where:{  id: id}
+                })
+            return res.status(204).send()
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json(error.massage)
+        }
     }
 }
--- src/routes/routers.js
+++ src/routes/routers.js
@@ -3,5 +3,6 @@ const route = express();
 const userController = require('../controllers/userController')
 
 route.post('/user', userController.create)
+route.delete('/user/:id', userController.destroy)
 
 module.exports = route;
